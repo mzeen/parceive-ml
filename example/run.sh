28dbb412848f840fc6b8996dac5c04d163d81d86
Andreas Wilhelm <andreas.wilhelm@in.tum.de>
2017-04-18 09:13:58 +0200

    adjusted run script

--- example/run.sh
+++ example/run.sh
@@ -6,7 +6,7 @@ do
         for weight in 10 50 100 200
         do
             TEST="test_example_${variance}_${ntasks}_${weight}" 
-            pin -t ~/workspace/parceive/dc_cpp/build/Release/libParceive.so -db ${TEST}.db -f filter.xml -mode analyze -accesses false  -- ./test_example $variance $ntasks $weight
+            /shared/pin-2.14/intel64/bin/pinbin -injection child -t /shared/parceive/dc_cpp/release/lib/libParceive.so -db ${TEST}.db -f filter.xml -variable false -loop on -sourcelocation false  -- ./test_example $variance $ntasks $weight
             sqlite3 -header -csv  ${TEST}.db < calls.sql > ${TEST}.csv
             sqlite3 -csv ${TEST}.db < loopcalls.sql >> ${TEST}.csv
         done 
